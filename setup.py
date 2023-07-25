a28bc719f801fd04977481bdd4d5d85ebcd5005b
Karthik Satheesh Kumar <49326615+KarthikSKumar98@users.noreply.github.com>
2023-07-25 10:08:42 -0700

    Update setup.py
    Updated release version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as fh:
 
 setuptools.setup(
     name="pycentral",
-    version="0.0.3",
+    version="0.0.4",
     author="aruba-automation",
     author_email="aruba-automation@hpe.com",
     description="Aruba Central Python Package",
